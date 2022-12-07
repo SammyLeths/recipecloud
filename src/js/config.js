01709ebf2a4b9ecb8810a078e43b4a1ad3a7e68a
SammyLeths <s.o.eyiowuawi@gmail.com>
2022-12-07 17:21:06 +0100

    initial deployment

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '67f6a562-0982-407a-a615-165206e909bc';
+export const MODAL_CLOSE_SEC = 2.5;
